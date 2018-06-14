a55b9f05553c27880bdfb33ed02ccb089780eef5
Robin Bron <robin@finwo.nl>
2018-06-14 15:28:36 +0200

    Chose picodb as orm ; removed unneeded dependencies

--- middleware/auth/jwt.php
+++ middleware/auth/jwt.php
@@ -1,5 +1,23 @@
 <?php
 
-$GLOBALS['router']->respond(function() {
-    return 'All the things';
+$GLOBALS['router']->respond(function () {
+
+    // Detect auth
+    $auth = false;
+    $auth = isset($_GET['token']) ? $_GET['token'] : false;
+    $auth = isset($_SERVER['HTTP_AUTHORIZATION']) ? $_SERVER['HTTP_AUTHORIZATION'] : $auth;
+    if ($auth === false) {
+        return;
+    }
+
+    // Strip possible prefixes
+    $prefixes = array("Bearer ", "Token ");
+    foreach ($prefixes as $prefix) {
+        if (substr($auth, 0, strlen($prefix)) === $prefix) {
+            $auth = substr($auth, strlen($prefix));
+        }
+    }
+
+    // Check
+    var_dump('dinges');
 });
