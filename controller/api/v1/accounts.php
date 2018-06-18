6278f262bbe7dbbbb7a9dbdcdf402e883171895e
Robin Bron <robin@finwo.nl>
2018-06-18 13:34:48 +0200

    Fetching accounts now works

--- controller/api/v1/accounts.php
+++ controller/api/v1/accounts.php
@@ -1,7 +1,8 @@
 <?php
-
 /** @var \Klein\Klein $router */
-$router->respond('/api/v1/accounts',function () {
+
+// Fetch all accounts
+$router->respond('GET','/api/v1/accounts',function () {
     global $_SERVICE;
     $isAdmin = isset($_REQUEST['auth']['account']['settings']['admin']) ? $_REQUEST['auth']['account']['settings']['admin'] : false;
 
@@ -12,10 +13,31 @@ $router->respond('/api/v1/accounts',function () {
 
     /** @var \PicoDb\Database $odm */
     $odm      = $_SERVICE['odm'];
-    $accounts = $odm->table('account')->findAll();
+    $accounts = $odm->table('account')->findAllByColumn('username');
     header("Content-Type: application/json");
-    die(json_encode(array_map(function($account) {
-        $account['settings'] = json_decode($account['settings'],true);
-        return $account;
-    }, $accounts)));
+    die(json_encode($accounts));
+});
+
+// Fetch single account
+$router->respond('GET', '/api/v1/accounts/[:username]', function( $request ) {
+    global $_SERVICE;
+    $isAdmin   = isset($_REQUEST['auth']['account']['settings']['admin']) ? $_REQUEST['auth']['account']['settings']['admin'] : false;
+    $fetchSelf = $_REQUEST['auth']['account']['username'] === $request->param('username', false);
+
+    // Only admins may fetch anyone
+    if (!($fetchSelf||$isAdmin)) {
+        $_REQUEST['status'] = 403;
+        die($_SERVICE['template']('denied'));
+    }
+
+    /** @var \PicoDb\Database $odm */
+    $odm     = $_SERVICE['odm'];
+    $account = $odm->table('account')->eq('username',$request->param('username'))->findOne();
+    if(is_null($account)) {
+        $_REQUEST['status'] = 404;
+        header("Content-Type: application/json");
+        die('{"error":404,"description":"The requested entity could not be found"}');
+    }
+    $account['settings'] = json_decode($account['settings'], true);
+    die(json_encode($account));
 });
