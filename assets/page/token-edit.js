ad78aff9c0602b6e068ed4691f54d80e361fbcef
Robin Bron <robin@finwo.nl>
2018-06-19 17:08:42 +0200

    Added dialog for mapping deletion

--- assets/page/token-edit.js
+++ assets/page/token-edit.js
@@ -10,6 +10,25 @@ rv.data.form.account = { isAdmin: false };
 rv.data.form.tokenid = token.id;
 rv.data.form.rules   = [];
 
+// Upgrade a dialog after rivets
+function fancyDialog(target) {
+  if(Array.isArray(target)) return target.map(fancyDialog);
+  if(target.getAttribute('upgraded')) return true;
+  target.setAttribute('upgraded',1);
+  var cover = document.createElement('DIV');
+  // var close = document.createElement('I');
+  cover.className = 'cover';
+  // close.className = 'close material-icons';
+  // close.innerHTML = 'close';
+  target.parentNode.insertBefore(cover,target);
+  target.parentNode.insertBefore(target,cover);
+  // target.appendChild(close);
+  _(target).find('.close').on('click', function() {
+    target.close();
+  });
+  return true;
+}
+
 // RV actions
 rv.data.form.addrule = function( event, context ) {
   context.form.rules.push({
@@ -22,9 +41,24 @@ rv.data.form.addrule = function( event, context ) {
   });
 };
 rv.data.form.delrule = function( event, context ) {
-  var index = context['%rule%'];
-  context.form.rules.splice(index,1);
-  // TODO: del through API
+  var btn = event.target;
+  while(btn.tagName!=='BUTTON') btn = btn.parentNode;
+  var dialog = btn.value === 'confirm' ? btn : btn.nextElementSibling;
+  while(dialog.tagName !== 'DIALOG') dialog = dialog.parentNode;
+
+  // Confirmed = go ahead
+  if ( btn.value === 'confirm' ) {
+    dialog.close();
+    // TODO: API CALL
+    var index = context['%rule%'];
+    context.form.rules.splice(index,1);
+    return false;
+  }
+
+  // Show the confirmation dialog
+  fancyDialog(dialog);
+  dialog.show();
+  return false;
 };
 rv.data.form.saverule = function( event, context ) {
   console.log(context.rule);
