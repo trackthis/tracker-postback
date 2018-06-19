282722e096cc110e90b23eec1b0760524b189685
Robin Bron <robin@finwo.nl>
2018-06-19 11:41:39 +0200

    Added 'for' helper

--- service/template.php
+++ service/template.php
@@ -1,6 +1,7 @@
 <?php
 
 $_SERVICE['template'] = function ($c) {
+
     return function ($name, $data = array()) {
 
         static $engine = null;
@@ -15,9 +16,22 @@ $_SERVICE['template'] = function ($c) {
                     array('extension' => 'hbs')
                 ),
                 'helpers' => new \Handlebars\Helpers(array(
-                    'implode' => function() {
-                        var_dump(func_get_args());
-                        return '';
+                    'for' => function( \Handlebars\Template $template, \Handlebars\Context $context, \Handlebars\Arguments $arguments, $block ) {
+                        $ctx  = $context->get('this'); $out  = '';
+                        $list = $context->get($arguments->getPositionalArguments()[0]);
+                        $args = function( $index, $entry ) use ($ctx) {
+                            return array_merge($ctx,$entry,array('@index'=>$index));
+                        };
+                        if ( $arguments->getPositionalArguments()[1] === 'as' ) {
+                            $name = $arguments->getPositionalArguments()[2];
+                            $args = function( $index, $entry ) use ( $ctx, $name ) {
+                                return array_merge( $ctx, array( '@index' => $index, $name => $entry ));
+                            };
+                        }
+                        foreach ($list as $i => $entry) {
+                            $out .= $template->render($args($i,$entry));
+                        }
+                        return $out;
                     }
                 ))
             ));
@@ -25,8 +39,10 @@ $_SERVICE['template'] = function ($c) {
 
 
         if ('array' !== gettype($data)) $data = array();
+        $query              = array_merge($_GET,$_POST);
         $data['__pageName'] = $name;
         $data['__query']    = '';
+        $data['__token']    = isset($query['token']) ? $query['token'] : '';
         $params             = explode('?', $_SERVER['REQUEST_URI'], 2);
         array_shift($params);
         if(count($params)) $data['__query'] = array_shift($params);
