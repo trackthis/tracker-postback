f63f13c89e3807a27fedaa881bf717659eed31cf
Robin Bron <robin@finwo.nl>
2018-06-19 13:50:58 +0200

    Showing tokens again

--- controller/api/v1/tokens.php
+++ controller/api/v1/tokens.php
@@ -4,20 +4,37 @@
 // Read all tokens
 $router->respond('GET', '/api/v1/tokens', function () {
     global $_SERVICE;
-    $isAdmin = isset($_REQUEST['auth']['account']['settings']['admin']) ? $_REQUEST['auth']['account']['settings']['admin'] : false;
+    $settings          = $_REQUEST['auth']['account']['settings'];
+    $settings['admin'] = isset($settings['admin']) ? $settings['admin'] : false;
+    $settings['token'] = isset($settings['token']) ? $settings['token'] : false;
+    $isAdmin  = isset($settings['admin']) ? $settings['admin'] : false;
     header("Content-Type: application/json");
 
-    if (!$isAdmin) {
-        $_REQUEST['status'] = 403;
-        die('{"error":403,"description":"Permission denied"}');
+    // Start building the query
+    /** @var \PicoDb\Database $odm */
+    $odm   = $_SERVICE['odm'];
+    $query = $odm->table('token');
+
+    // Add username filter if needed
+    if ( isset($_GET['account']) ) {
+        if ( ($_GET['account']!==$_REQUEST['auth']['account']['username']) && (!$isAdmin) ) {
+            $_REQUEST['status'] = 403;
+            die('{"error":403,"description":"Permission denied"}');
+        }
+        $query = $query->eq('username', $_GET['account']);
+    } elseif(!$isAdmin) {
+        $query = $query->eq('username', $_REQUEST['auth']['account']['username']);
     }
 
-    /** @var \PicoDb\Database $odm */
-    $odm = $_SERVICE['odm'];
-    die(json_encode(array_map(function ($token) {
-        return array(
+    die(json_encode(array_map(function ($token) use ($settings) {
+        $result = array(
             'id'          => $token['id'],
             'description' => $token['description'],
+            'expires'     => $token['expires'],
         );
-    }, $odm->table('token')->findAll())));
+        if ( $settings['token'] ) {
+            $result['token'] = $token['token'];
+        }
+        return $result;
+    }, $query->findAll())));
 });
