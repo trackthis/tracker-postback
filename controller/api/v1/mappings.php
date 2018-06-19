37125719f62d604203c2a8ebf6b0a759da2ac837
Robin Bron <robin@finwo.nl>
2018-06-19 16:41:24 +0200

    Added fetch mappings api

---
+++ controller/api/v1/mappings.php
@@ -0,0 +1,38 @@
+<?php
+/** @var \Klein\Klein $router */
+
+// Read all tokens
+$router->respond('GET', '/api/v1/mappings/[i:tokenid]', function ( \Klein\Request $request ) {
+    global $_SERVICE;
+    $settings          = $_REQUEST['auth']['account']['settings'];
+    $settings['admin'] = isset($settings['admin']) ? $settings['admin'] : false;
+    $settings['token'] = isset($settings['token']) ? $settings['token'] : false;
+    $isAdmin  = isset($settings['admin']) ? $settings['admin'] : false;
+    header("Content-Type: application/json");
+
+    // Start building the token query
+    /** @var \PicoDb\Database $odm */
+    $odm   = $_SERVICE['odm'];
+    $query = $odm->table('token')->eq('id', $request->param('tokenid', 0));
+
+    // Add username filter if needed
+    if ( isset($_GET['account']) ) {
+        if ( ($_GET['account']!==$_REQUEST['auth']['account']['username']) && (!$isAdmin) ) {
+            $_REQUEST['status'] = 403;
+            die('{"error":403,"description":"Permission denied"}');
+        }
+        $query = $query->eq('username', $_GET['account']);
+    } elseif(!$isAdmin) {
+        $query = $query->eq('username', $_REQUEST['auth']['account']['username']);
+    }
+
+    // Fetch the token
+    $token = $query->findOne();
+    if(is_null($token)) {
+        $_REQUEST['status'] = 404;
+        die('{"error":404,"description":"Not found"}');
+    }
+
+    // Being here means we're allowed to fetch mappings
+    die(json_encode($odm->table('mapping')->eq('token', $token['id'])->findAll()));
+});
