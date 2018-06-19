e868c47ec5217bf098e98075b39de97da90fa763
Robin Bron <robin@finwo.nl>
2018-06-19 15:35:40 +0200

    Working on the token-edit page

--- controller/tokens.php
+++ controller/tokens.php
@@ -1,6 +1,28 @@
 <?php
-
 /** @var \Klein\Klein $router */
+
+
 $router->respond('/tokens',function () {
-    echo 'Yo, USER<br />This is the token management page';
+    global $_SERVICE;
+
+    die($_SERVICE['template']('token-overview'));
+});
+
+$router->respond('GET', '/tokens/[i:id]', function( \Klein\Request $request ) {
+    global $_SERVICE;
+
+    die($_SERVICE['template']('token-edit', array(
+        'title' => 'Edit token: ' . '--[description]--',
+        'edit'  => true,
+        'token' => $request->param('id', false ),
+    )));
+});
+
+$router->respond('GET', '/tokens/new', function() {
+    global $_SERVICE;
+
+    die($_SERVICE['template']('token-edit', array(
+        'title' => 'New token',
+        'new'   => true,
+    )));
 });
