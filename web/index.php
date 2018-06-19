b5692e28cb5efefa38f51d7193678442b4d2d002
Robin Bron <robin@finwo.nl>
2018-06-19 12:25:58 +0200

    Prevent double declaration of prnt

--- web/index.php
+++ web/index.php
@@ -2,32 +2,34 @@
 
 // DEBUG function
 // outputs yaml-like structure
-function prnt( $data, $ret = false, $prefix = '' ) {
-    $output = '';
-    foreach ( $data as $key => $value ) {
-        $output .= $prefix . $key . ':';
-        switch(gettype($value)) {
-            case 'boolean':
-                $output .= ' ' . ($value?'true':'false') . PHP_EOL;
-                break;
-            case 'string':
-            case 'integer':
-            case 'number':
-            case 'float':
-            case 'double':
-                $output .= ' ' . $value . PHP_EOL;
-                break;
-            case 'array':
-                $output .= PHP_EOL;
-                $output .= prnt( $value, true, $prefix.'  ');
-                break;
+if(!function_exists('prnt')) {
+    function prnt( $data, $ret = false, $prefix = '' ) {
+        $output = '';
+        foreach ( $data as $key => $value ) {
+            $output .= $prefix . $key . ':';
+            switch(gettype($value)) {
+                case 'boolean':
+                    $output .= ' ' . ($value?'true':'false') . PHP_EOL;
+                    break;
+                case 'string':
+                case 'integer':
+                case 'number':
+                case 'float':
+                case 'double':
+                    $output .= ' ' . $value . PHP_EOL;
+                    break;
+                case 'array':
+                    $output .= PHP_EOL;
+                    $output .= prnt( $value, true, $prefix.'  ');
+                    break;
+            }
         }
-    }
-    if ($ret) {
-        return $output;
-    } else {
-        echo $output;
-        return null;
+        if ($ret) {
+            return $output;
+        } else {
+            echo $output;
+            return null;
+        }
     }
 }
 
