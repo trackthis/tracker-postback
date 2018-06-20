7ee15f44fb3f5cc8abec22c46c55d612bd54cde4
Robin Bron <robin@finwo.nl>
2018-06-20 13:40:00 +0200

    Functional 2-way datetime binder

--- assets/lib/rivets.js
+++ assets/lib/rivets.js
@@ -22,6 +22,25 @@ rivets.formatters.append = function( origin, arg ) {
   return origin + arg;
 };
 
+function flen(n,l) {
+  return ('0'.repeat(l||2)+n).slice(-(l||2));
+}
+
+rivets.formatters.datetime = {
+  read: function( value ) {
+    var date = (new Date(value * 1000));
+    return date.getFullYear()
+           + '-' + flen(date.getMonth() + 1)
+           + '-' + flen(date.getDate())
+           + 'T' + flen(date.getHours())
+           + ':' + flen(date.getMinutes())
+           + ':' + flen(date.getSeconds());
+  },
+  publish: function(value) {
+    return Date.parse(value) / 1000;
+  }
+};
+
 // Bind once the current run of code is done
 setTimeout(function(){
   rv.bindings = rivets.bind(document.body, rv.data).bindings;
