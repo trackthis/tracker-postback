e98f65b4bb8e9ed47a303f1ba662448e05c7b971
Robin Bron <robin@finwo.nl>
2018-06-21 13:23:52 +0200

    Adding JSON body parser

---
+++ middleware/auth/00-json-body.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Klein\Klein $router */
+$router->respond(function () {
+
+    // We need JSON & a broken $_POST for this middleware
+    if ( $_SERVER['HTTP_CONTENT_TYPE'] !== 'application/json' ) return;
+    if ( count($_POST) > 0 ) return;
+
+    $body = file_get_contents('php://input');
+    var_dump($body);
+
+});
