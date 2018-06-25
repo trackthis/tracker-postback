829b780d90b957dd3b84e12d440d047774ac0aab
Robin Bron <robin@finwo.nl>
2018-06-25 21:42:03 +0200

    Sorry, mysql wants a formatted date for timestamps

--- src/Tracker/Translator/Transform/StringToTime.php
+++ src/Tracker/Translator/Transform/StringToTime.php
@@ -4,7 +4,7 @@ namespace Tracker\Translator\Transform;
 
 class StringToTime implements TransformInterface {
     public function handle($argv,$value) {
-        return strtotime($value);
+        return gmdate('Y-m-d H:i:s',strtotime($value));
     }
     public static function getName() {
         return 'strtotime';
