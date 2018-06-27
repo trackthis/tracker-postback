eb16f159c46cc03eb21b5b643f56f0002545465e
Robin Bron <robin@finwo.nl>
2018-06-27 17:42:55 +0200

    Add JSON body support in the HTTP writer

--- src/Finwo/Pipe/Writer/HttpWriter.php
+++ src/Finwo/Pipe/Writer/HttpWriter.php
@@ -12,7 +12,7 @@ class HttpWriter extends AbstractWriter {
     public function write($data) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->target);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
@@ -20,13 +20,20 @@ class HttpWriter extends AbstractWriter {
         $output  = curl_exec($ch);
         $info    = curl_getinfo($ch);
         curl_close($ch);
-//        if($info['http_code']!==200) {
-            var_dump($data);
-            var_dump($info);
-            var_dump($output);
-            die();
-//        }
-        return ($info['http_code'] === 200);
+        if($info['http_code']!==200) return false;
+
+        // Detect JSON ok in body
+        if($info['content_type']=='application/json') {
+            try {
+                $response = json_decode($output,true);
+                var_dump($response);
+                if(key_exists('ok',$response) && (!$response['ok'])) return false;
+            } catch( \Exception $e ) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
