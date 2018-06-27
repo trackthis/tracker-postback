06ae2b4d52b57fc670918c3f5abc3d496cc3acce
Robin Bron <robin@finwo.nl>
2018-06-27 17:18:49 +0200

    Allow custom source for MD5

--- src/Tracker/Translator/Transform/MD5.php
+++ src/Tracker/Translator/Transform/MD5.php
@@ -4,6 +4,7 @@ namespace Tracker\Translator\Transform;
 
 class MD5 implements TransformInterface {
     public function handle($argv,$value) {
+        if(count($argv)) { $value = implode('',$argv); }
         return md5($value);
     }
     public static function getName() {
