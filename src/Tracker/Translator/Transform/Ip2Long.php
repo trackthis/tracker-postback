bc1ba4ab4104e1d6603d07de3c9092eb2ee836c0
Robin Bron <robin@finwo.nl>
2018-06-28 18:02:43 +0200

    These transforms should implode with spaces

--- src/Tracker/Translator/Transform/Ip2Long.php
+++ src/Tracker/Translator/Transform/Ip2Long.php
@@ -4,7 +4,7 @@ namespace Tracker\Translator\Transform;
 
 class Ip2Long implements TransformInterface {
     public function handle($argv,$value) {
-        if(count($argv)) { $value = implode('',$argv); }
+        if(count($argv)) { $value = implode(' ',$argv); }
         return ip2long($value);
     }
     public static function getName() {
