262e5c000d0451255be670931da39277cce75855
Robin Bron <robin@finwo.nl>
2018-08-02 17:57:04 +0200

    Catch ip2long false

--- src/Tracker/Translator/Transform/Ip2Long.php
+++ src/Tracker/Translator/Transform/Ip2Long.php
@@ -5,7 +5,7 @@ namespace Tracker\Translator\Transform;
 class Ip2Long implements TransformInterface {
     public function handle($argv,$value) {
         if(count($argv)) { $value = implode(' ',$argv); }
-        return ip2long($value);
+        return ip2long($value) ?: 0;
     }
     public static function getName() {
         return 'ip2long';
