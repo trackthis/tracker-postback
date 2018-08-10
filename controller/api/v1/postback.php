48a3261cee0a2a626a17f5a82ba65ef27db4b4f8
Robin Bron <robin@finwo.nl>
2018-08-10 15:07:46 +0200

    Added guid to the input

--- controller/api/v1/postback.php
+++ controller/api/v1/postback.php
@@ -25,6 +25,29 @@ $router->respond('GET', '/api/v1/postback', function ( \Klein\Request $request )
         ));
     }
 
+    // Another helper function
+    function guid() {
+        if(function_exists('com_create_guid')) {
+            return trim(com_create_guid(),'{}');
+        } else if (function_exists('uuid_create')) {
+            return uuid_create();
+        } else if (function_exists('openssl_random_pseudo_bytes')) {
+            $data = openssl_random_pseudo_bytes(16);
+            $data[6] = chr(ord($data[6]) & 0x0f | 0x40);    // set version to 0100
+            $data[8] = chr(ord($data[8]) & 0x3f | 0x80);    // set bits 6-7 to 10
+            return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+        } else {
+          mt_srand((double)microtime() * 10000);
+          $charid = strtolower(md5(uniqid(rand(), true)));
+          $hyphen = chr(45); // "-"
+          return substr($charid,  0,  8).$hyphen.
+                 substr($charid,  8,  4).$hyphen.
+                 substr($charid, 12,  4).$hyphen.
+                 substr($charid, 16,  4).$hyphen.
+                 substr($charid, 20, 12);
+        }
+    }
+
     // Fetch the target
     $token      = $_REQUEST['auth']['token'];
     $defaultUri = db2uri(\Finwo\Framework\Config\Config::get('tracker.default'));
@@ -43,7 +66,8 @@ $router->respond('GET', '/api/v1/postback', function ( \Klein\Request $request )
     $adapter->setFields($_REQUEST['auth']['token']['mappings']);
 
     // Let's write what we got
-    $message = $adapter->record($request->params());
+    $data = array_merge(array('guid'=>guid()),$request->params());
+    $message = $adapter->record($data);
     if($message) {
         $code = $message == 'idx_unique' ? 400 : 500;
         http_response_code($code);
